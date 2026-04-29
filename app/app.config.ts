7f98b47840c19ae7adfad9327dc1e11689d2fcbb
Iyan Sanjaya <iyan.sanjaya1@gmail.com>
2026-04-29 11:22:16 +0700

    update

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,9 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
+  title: 'Luns',
+  github: '/',
+  coffee: '/',
+  twitter: '/',
+  telegram: '/',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
